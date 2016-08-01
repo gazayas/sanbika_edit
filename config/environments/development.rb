afd9a0292ae44561fa92eeec5731c8d393aead1f
gazayas <g-zayas@hotmail.com>
2016-08-01 19:25:47 +0900

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,18 +13,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true # デフォルトは false
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => '587',
-    :domain => 'sanbika.herokuapp.com',
-    :authentication => 'plain',
-    :user_name => ENV['MAIL_USER_NAME'],
-    :password => ENV['MAIL_PASSWORD']
-}
+        address:        "smtp.gmail.com",
+        port:           587,
+        authentication: "plain",
+        user_name:      ENV['MAIL_USER_NAME'],
+        password:       ENV['MAIL_PASSWORD'],
+        domain:         'sanbika.herokuapp.com',
+        enable_starttls_auto: true
+    }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,17 +63,16 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true # デフォルトは false
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '25',
-    :authentication => :plain,
-    :user_name      => ENV['MAIL_USER_NAME'],
-    :password       => ENV['MAIL_PASSWORD'],
-    :domain         => 'sanbika.herokuapp.com',
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = {
+        address:        "smtp.gmail.com",
+        port:           587,
+        authentication: "plain",
+        user_name:      ENV['MAIL_USER_NAME'],
+        password:       ENV['MAIL_PASSWORD'],
+        domain:         'sanbika.herokuapp.com',
+        enable_starttls_auto: true
+    }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
