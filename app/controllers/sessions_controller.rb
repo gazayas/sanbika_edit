13feab8de419b43c8362c2e456c56e608fc2875f
gazayas <g-zayas@hotmail.com>
2016-08-06 10:01:02 +0900

    usersにadminの属性を追加しました

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
     end
   end
 
+
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :status, :admin)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :status)
   end
 
 
