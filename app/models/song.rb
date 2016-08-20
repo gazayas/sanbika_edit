4747733e9edc43dcf6e25763aaacc80c8b5a0271
gazayas <g-zayas@hotmail.com>
2016-08-20 09:45:39 +0900

    リストをBoostrapで変えました

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
 
   validates :title, presence: true, length: {maximum: 30}
   validates :title_yomikata, length: {maximum: 40}
-  validates :artist, length: {maximum: 30}
+  validates :artist, length: {maximum: 30} # これを 20 にした方がいいかな
   validates :artist_yomikata, length: {maximum: 30}
   validates :body, presence: true, length: {maximum: 3000}
 
