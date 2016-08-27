e38e673534cfb1ef4c28b4f3a80ed8ce512567eb
gazayas <g-zayas@hotmail.com>
2016-08-27 10:39:26 +0900

    seeds.rbを更新したけど、songs/index.htmlは大変なことになってるので変えたいです

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-=begin
-User.create!(
-  name: "gabe",
-  email: "gabe@example.com",
-  password: "gabe",
-  password_confirmation: "gabe",
-  activated: false,
-  activated_at: Time.zone.now
-)
-
-
 99.times do |n|
   name  = "user#{n}"
   email = "example-#{n+1}@example.org"
-  password = "user"
-  User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+  password = "user#{n}"
+  user = User.create!(
+    name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now,
+    )
+  1.times do |i|
+    user.songs.create!(
+      title: "賛美歌#{i}",
+      title_yomikata: "さんびか#{i}",
+      key: "C",
+      body: "*C*   *Am*   *F*   *Cb*<br/><br/>賛美歌の歌詞はここに入ります"
+    )
+  end
+  user
 end
-=end
